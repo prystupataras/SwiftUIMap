778210391ea0eef0c3ddc30c0fdf26c50f4b8763
Taras <45391115+prystupataras@users.noreply.github.com>
2024-11-05 10:06:55 +0200

    Initial Commit

---
+++ SwiftUIMap/SwiftUIMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIMapApp.swift
+//  SwiftUIMap
+//
+//  Created by Taras Prystupa on 05.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
